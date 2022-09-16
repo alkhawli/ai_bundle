af36be8179f593b7052d7b12c27cfc70e8b3dbeb
Toufik Al Khawli <toufik.al.khawli@pwc.com>
2022-09-16 17:35:10 +0200

    Added flake

--- ai_bundle/main.py
+++ ai_bundle/main.py
@@ -1,2 +1,18 @@
-a = 1
-print(a)
+# import os
+
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+# and os.getenv("DEBUG", "0") == 1
+
+if __name__ == "__main__":
+
+    uvicorn.run(app, host="0.0.0.0", port=5000, debug=True)
